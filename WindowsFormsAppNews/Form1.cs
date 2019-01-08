2fba206cf774706d18f5a9a66c7c2f49a086a128
sercep <sercep98@gmail.com>
2019-01-09 00:45:41 +0300

    add 2 comboboxes

--- WindowsFormsAppNews/Form1.cs
+++ WindowsFormsAppNews/Form1.cs
@@ -16,5 +16,9 @@ namespace WindowsFormsAppNews
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
     }
 }
